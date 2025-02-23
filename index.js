d6af7230fca9a2bea92fd0d5757f299a79e83673
owinckle <oceanwinckler.rem@gmail.com>
2025-02-23 18:28:04 +0100

    command

--- index.js
+++ index.js
@@ -9,16 +9,26 @@ program
 	.version("0.0.1");
 
 // Log function
-function log(message) {
+function logSuccess(message) {
 	console.log(`\x1b[32mStableHooks ➜ \x1b[0m${message}`);
+}
+
+function logError(message) {
+	console.error(`\x1b[31mStableHooks ➜ \x1b[0m${message}`);
 }
 
 // Listen command
 program
-	.command("listen <key>")
+	.command("listen")
 	.description("Listen for incoming webhooks")
-	.action((key) => {
-		log(`Listening for incoming webhooks with key: ${key}`);
+	.option("--key <key>", "Key for listening to webhooks")
+	.action((options) => {
+		const { key } = options;
+		if (!key) {
+			logError("--key is required");
+			process.exit(1);
+		}
+		logSuccess(`Listening for incoming webhooks with key: ${key}`);
 	});
 
 program.parse(process.argv);
